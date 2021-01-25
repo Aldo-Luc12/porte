cbed773a936a0e69b36ae6b47b7f8125c2c1527b
Aldo-luc <57046398+Aldo-luc@users.noreply.github.com>
2021-01-25 07:17:40 +0300

    init

---
+++ wifistyle/script.js
@@ -0,0 +1,11 @@
+//connexion
+
+var noms, mdps, resultat;
+function result(){
+    noms = document.forms["entree"]["name"].value;
+    mdps = document.forms["entree"]["pass"].value;
+    if(noms==rakoto && mdps==tsikaiaby){
+        document.write("okay");
+        href="http:www.google.com"
+    }
+}
